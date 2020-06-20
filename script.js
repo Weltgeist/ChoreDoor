422538fb349aa5becf992dbb4e4f696b29da3332
Weltgeist <narcisse118@gmail.com>
2020-06-20 02:23:58 -0400

    Bug fix, called function

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-
 const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
 const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
 let numCloseDoors = 3;
-let openDoor1;
-let openDoor2;
-let openDoor3;
+let openDoor1 = "";
+let openDoor2 = "";
+let openDoor3 = "";
 randomChoreDoorGenerator = () => {
   let choreDoor = Math.floor((Math.random()*numCloseDoors));
   switch (choreDoor) {
@@ -30,6 +30,7 @@ randomChoreDoorGenerator = () => {
       break;
   }
 };
+randomChoreDoorGenerator();
 doorImage1.onclick = () => doorImage1.src = openDoor1;
 doorImage2.onclick = () => doorImage2.src = openDoor2;
 doorImage3.onclick = () => doorImage3.src = openDoor3;
