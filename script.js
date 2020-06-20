8677f3c75dca05959443a347b9e3a0a3f34c6f92
Weltgeist <narcisse118@gmail.com>
2020-06-20 02:10:10 -0400

    added randomchore+ function

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const doorImage3 = document.getElementById("door3");
 const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
 const beachDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
 const spaceDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+let numCloseDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+randomChoreDoorGenerator = () => {
+  let choreDoor = Math.floor((Math.random()*numCloseDoors));
+};
 doorImage1.onclick = () => doorImage1.src = botDoorPath;
 doorImage2.onclick = () => doorImage2.src = beachDoorPath;
 doorImage3.onclick = () => doorImage3.src = spaceDoorPath;
